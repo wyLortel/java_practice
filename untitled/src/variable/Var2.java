e1c15e36580f73ef98725c196cdf562209e3abda
JEOUNG WOOYOUNG <young99@g.yju.ac.kr>
2025-05-06 13:50:58 +0900

    first commit

---
+++ untitled/src/variable/Var2.java
@@ -0,0 +1,11 @@
+package variable;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Var2 {
+    public static void main(String[] args) {
+        System.out.println(20);
+        System.out.println(20);
+        System.out.println(20);
+    }
+}
