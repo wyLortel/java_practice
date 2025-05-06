93880a3be9085698c4fea96f88ac4a2eb42c9159
JEOUNG WOOYOUNG <young99@g.yju.ac.kr>
2025-05-06 15:58:24 +0900

    kakunin intelijei commit

---
+++ untitled/src/operator/operator2.java
@@ -0,0 +1,22 @@
+package operator;
+
+public class operator2 {
+    public static void main(String[] args) {
+
+        String result1 = "hello" + "wolrd";
+        System.out.println(result1);
+
+        String S1 = "string1";
+        String S2 = "string2";
+        String result2 = S1 + S2;
+        System.out.println(result2);
+
+        String result3 = "a + b = " + 10;
+        System.out.println(result3);
+
+        int num = 20;
+        String str = "a + b = ";
+        String result4 = str + num;
+        System.out.println(result4);
+    }
+}
