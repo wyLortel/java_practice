f7b7975904edb31a1d0010a1833fd0b92bbec67b
wowowoyoung <young99@g.yju.ac.kr>
2025-08-03 23:23:58 +0900

    2025-08-03

---
+++ javaMiddel/src/lang/immutable/address/ReMain1_2.java
@@ -0,0 +1,18 @@
+package lang.immutable.address;
+
+public class ReMain1_2 {
+
+    public static void main(String[] args) {
+        Address a = new Address("서울");
+        Address b = new Address("서울");
+
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        b.value = "부산";
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
