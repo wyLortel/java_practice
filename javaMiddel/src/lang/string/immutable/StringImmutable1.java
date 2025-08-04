db0ef5d58951f51d548c8888d3e558104847306f
JEOUNG WOOYOUNG <young99@g.yju.ac.kr>
2025-08-04 17:42:29 +0900

    2025-08-04

---
+++ javaMiddel/src/lang/string/immutable/StringImmutable1.java
@@ -0,0 +1,10 @@
+package lang.string.immutable;
+
+public class StringImmutable1 {
+
+    public static void main(String[] args) {
+        String string = "hello";
+        string.concat("java");
+        System.out.println("string = " + string);
+    }
+}
---
+++ javaMiddel/src/lang/string/immutable/StringImmutable2.java
@@ -0,0 +1,11 @@
+package lang.string.immutable;
+
+public class StringImmutable2 {
+
+    public static void main(String[] args) {
+        String string = "hello";
+        String string2 = string.concat("java");
+        System.out.println("string = " + string);
+        System.out.println("string2 = " + string2);
+    }
+}
